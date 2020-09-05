383a76dd60d1c9949d89c060373fe88968bc2b41
firaga <jichen@mafengwo.cn>
2020-09-05 17:29:56 +0800

    context

--- src/main/context.go
+++ src/main/context.go
@@ -1,32 +1,40 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"sync"
 	"time"
 )
 
-func func1() error {
+func func1(ctx context.Context, wg *sync.WaitGroup) error {
+	defer wg.Done()
 	respC := make(chan int)
 	// 处理逻辑
 	go func() {
-		time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 5)
 		respC <- 10
-		close(respC)
 	}()
-
-	// 超时逻辑
+	// 取消机制
 	select {
+	case <-ctx.Done():
+		fmt.Println("cancel")
+		return errors.New("cancel")
 	case r := <-respC:
-		fmt.Printf("Resp: %d\n", r)
+		fmt.Println(r)
 		return nil
-	case <-time.After(time.Second * 2):
-		fmt.Println("catch timeout")
-		return errors.New("timeout")
 	}
 }
 
 func main() {
-	err := func1()
-	fmt.Printf("func1 error: %v\n", err)
+	wg := new(sync.WaitGroup)
+	ctx, cancel := context.WithCancel(context.Background())
+	wg.Add(1)
+	go func1(ctx, wg)
+	time.Sleep(time.Second * 2)
+	// 触发取消
+	cancel()
+	// 等待goroutine退出
+	wg.Wait()
 }
